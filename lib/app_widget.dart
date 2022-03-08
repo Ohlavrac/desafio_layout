225556eacc59b7eb28bd52fa6a576e459c38cad0
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-03-08 10:22:06 -0300

    initial app setup

---
+++ lib/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'modules/main_page/main_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Desafio Layout",
+      initialRoute: "/main_page",
+      routes: {
+        "/main_page": (context) => MainPage(),
+      },
+    );
+  }
+}
