db9b5a2b851fc490acbfcd88aea74ac3f3970f9a
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-03-09 11:31:22 -0300

    add float action button

--- lib/modules/main_page/main_page.dart
+++ lib/modules/main_page/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:desafio_layout/shared/components/data_card/data_card.dart';
+import 'package:desafio_layout/shared/components/floatActionButtonAnim/floatActionButtonAnim.dart';
 import 'package:desafio_layout/shared/components/money_card/money_card.dart';
 import 'package:desafio_layout/shared/themes/app_text_styles.dart';
 import 'package:flutter/material.dart';
@@ -11,79 +12,169 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin {
   Icon _myIcon = Icon(Icons.visibility);
   bool _buttonClick = false;
+
+  late AnimationController animationController;
+  late Animation degOneTranslationAnimation;
+
+  double getRadiansFromDegree(double degree) {
+    double unitRadian = 57.295779513;
+    return degree / unitRadian;
+  }
+
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this,duration: Duration(microseconds: 250));
+    degOneTranslationAnimation = Tween(begin: 0.0, end: 1.0).animate(animationController);
+    super.initState();
+    animationController.addListener(() {
+      setState(() {
+        
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(25),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 30),
-                  child: Row(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(25),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 45,
+                          backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/57482542?v=4"),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Olá",
+                              style: TextStyles.textsSimple,
+                            ),
+                            Text(
+                              "Viktor!",
+                              style: TextStyles.texts,
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      CircleAvatar(
-                        radius: 45,
-                        backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/57482542?v=4"),
+                      Text(
+                        "Parabéns! Esse mês você fez",
+                        style: TextStyles.textsSimple,
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            "Olá",
-                            style: TextStyles.textsSimple,
-                          ),
-                          Text(
-                            "Viktor!",
-                            style: TextStyles.texts,
-                          )
-                        ],
-                      ),
+                      IconButton(
+                        icon: _myIcon,
+                        iconSize: 35,
+                        color: AppColors.options,
+                        onPressed: () {
+                          setState(() => {
+                            if (!_buttonClick) {
+                              _myIcon = Icon(Icons.visibility_off),
+                              _buttonClick = true
+                            } else {
+                              _myIcon = Icon(Icons.visibility),
+                              _buttonClick = false
+                            }
+                          });
+                        },
+                      )
                     ],
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Parabéns! Esse mês você fez",
-                      style: TextStyles.textsSimple,
-                    ),
-                    IconButton(
-                      icon: _myIcon,
-                      iconSize: 35,
-                      color: AppColors.options,
-                      onPressed: () {
-                        setState(() => {
-                          if (!_buttonClick) {
-                            _myIcon = Icon(Icons.visibility_off),
-                            _buttonClick = true
-                          } else {
-                            _myIcon = Icon(Icons.visibility),
-                            _buttonClick = false
-                          }
-                        });
-                      },
-                    )
-                  ],
-                ),
-                DataCard(),
-                SizedBox(height: 25,),
-                MoneyCard(),
-                SizedBox(height: 150,),
-              ],
+                  DataCard(),
+                  SizedBox(height: 25,),
+                  MoneyCard(),
+                  SizedBox(height: 150,),
+                ],
+              ),
             ),
           ),
-        ),
+          Positioned(
+            right: 10,
+            bottom: 10,
+            child: Stack(
+              children: [
+                Transform.translate(
+                  offset: Offset.fromDirection(getRadiansFromDegree(270), degOneTranslationAnimation.value * 100),
+                  child: CircularButton(
+                    color: AppColors.primary,
+                    width: 55,
+                    height: 55,
+                    icon: Icon(
+                      Icons.shop,
+                      color: Colors.white,
+                    ),
+                    onClick: () => {
+                      print("Shop")
+                    },
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset.fromDirection(getRadiansFromDegree(225), degOneTranslationAnimation.value * 100),
+                  child: CircularButton(
+                    color: AppColors.primary,
+                    width: 55,
+                    height: 55,
+                    icon: Icon(
+                      Icons.people,
+                      color: Colors.white,
+                    ),
+                    onClick: () => {
+                      print("Pessoa 2")
+                    },
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset.fromDirection(getRadiansFromDegree(180), degOneTranslationAnimation.value * 100),
+                  child: CircularButton(
+                    color: AppColors.primary,
+                    width: 55,
+                    height: 55,
+                    icon: Icon(
+                      Icons.people,
+                      color: Colors.white,
+                    ),
+                    onClick: () => {
+                      print("Pessoa")
+                    },
+                  ),
+                ),
+                CircularButton(
+                  color: AppColors.primary,
+                  width: 60,
+                  height: 60,
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                  onClick: () => {
+                    if (animationController.isCompleted) {
+                      animationController.reverse()
+                    } else {
+                      animationController.forward()
+                    }
+                  },
+                ),
+              ],
+          ))
+        ],
       ),
     );
   }
